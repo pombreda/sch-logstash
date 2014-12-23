e9672c56d2bc720b127f9df84e82c5f2466c7641
davidski <davidski@deadheaven.com>
2014-12-23 12:55:39 -0800

    correct operator on metadata

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ description      'Installs/Configures logstash'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 
-depends 'logstash', ">~ 0.11.0"
+depends 'logstash', "~> 0.11.0"
 depends 'chef-sugar'
